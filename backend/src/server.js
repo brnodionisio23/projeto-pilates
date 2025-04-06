eb2bebc03a45f437c2691c515a5622f6bdb1de5f
Breno <brnodionisio23@gmail.com>
2025-04-06 13:14:44 -0300

    Configurando banco de dados SQlite

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,23 @@
 const express = require('express')
+const sequelize = require('../config/db');
 const app = express()
 const port = 3000
 
-app.listen(port, () => {
-    console.log(`Servidor rodando na porta: ${port}`)
-})
+async function runServer() {
+    try {
+        await sequelize.authenticate();
+        console.log('Conexão com o banco de dados estabelecida com sucesso');
+
+        await sequelize.sync();
+        console.log('Tabelas sincronizadas com sucesso');
+
+        app.listen(port, () => {
+            console.log(`Servidor rodando na porta: ${port}`);
+        });
+
+    } catch (error) {
+        console.error('Erro ao iniciar o servidor:', error);
+    }
+}
+
+runServer();
